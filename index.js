77c22ac312da2f75dc8c77bad3cb6276f2fbe93d
R.A Borja <raborja.dev@gmail.com>
2022-11-04 00:52:24 +0800

    change sun icon to cloud

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const KEY = "54606dac77b8599c32e90d01077bfac0";
 
 async function getWeatherApiData(city) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${KEY}`;
+  const UNIT = "metric";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${KEY}&units=${UNIT}`;
   const response = await fetch(url, { mode: "cors" });
 
   if (response.ok) {
@@ -12,6 +13,16 @@ async function getWeatherApiData(city) {
   }
 }
 
-getWeatherApiData("London")
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+getWeatherApiData("London").then(data => console.log(data));
+
+function weather(obj) {
+  return {
+    city: obj.city,
+    dt: obj.dt,
+    temp: obj.main.temp,
+    weather: obj.weather.main,
+    wind: obj.wind.speed,
+    humid: obj.main.humidity,
+    clouds: obj.clouds.all,
+  };
+}
